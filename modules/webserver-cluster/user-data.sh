fd11b22b2fe16a621343bda558b72388d5c1d679
njendad1460 <davidnjesh1460@gmail.com>
2026-04-09 16:17:37 +0300

    sh update

--- modules/webserver-cluster/user-data.sh
+++ modules/webserver-cluster/user-data.sh
@@ -4,6 +4,6 @@ apt-get update -y
 apt-get install -y apache2
 systemctl start apache2
 systemctl enable apache2
-echo "<h1>Hello from ASG Instance: $(hostname -f) - No down time </h1>" > /var/www/html/index.html
+echo "<h1>Hello from ASG Instance: $(hostname -f) - No downi time </h1>" > /var/www/html/index.html
 # Notice ${server_port} — this will come from Terraform using templatefile.
 # Using a template file avoids the gotcha with inline paths.
